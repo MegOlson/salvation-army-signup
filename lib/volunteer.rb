d1162b64c2da017e7f34c784a318191195e45c90
mo <meganannetteolson@yahoo.com>
2017-10-26 16:46:22 -0700

    Add commented out callback.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,2 +1,8 @@
 class Volunteer < ActiveRecord::Base
+  # before_save(:format_date)
+  #
+  # def format_date
+  #   date = self.date.split("-")
+  #   self.date = date.rotate(1).join("-")
+  # end
 end
