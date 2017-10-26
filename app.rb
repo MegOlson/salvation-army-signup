402a6fb77ac88bcb925c92d6185f5d70dfc6c879
mo <meganannetteolson@yahoo.com>
2017-10-26 12:49:48 -0700

    Add form with name and date input.

--- app.rb
+++ app.rb
@@ -8,5 +8,11 @@ require("pg")
 DB = PG.connect({:dbname => "salvation_army_signup_test"})
 
 get('/') do
+  @volunteers = Volunteer.all();
   erb(:index)
+end
+
+post('/') do
+  @volunteers = Volunteer.all();
+  erb(:success)
 end
